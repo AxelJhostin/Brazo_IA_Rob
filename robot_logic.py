929721e757874762e07fd2cb6043bfce15771328
PaulaMarquez <paula.m@inpronicusa.com>
2025-11-20 10:51:56 -0500

    Cambio en el angulo en el que se mueve el hombro, el dedo 3, intercambio entre dedo 3 y 1.

--- robot_logic.py
+++ robot_logic.py
@@ -258,8 +258,8 @@ def _calcular_angulos_brazo(landmarks, h, w):
     if mag1 > 0 and mag2 > 0: 
         dot_product = np.dot(vec1, vec2)
         cos_angle = np.clip(dot_product / (mag1 * mag2), -1.0, 1.0)
-        ang_codo = np.degrees(np.arccos(cos_angle))
-    
+        ang_codo = 180 - np.degrees(np.arccos(cos_angle))
+        
     return ({'hombro': ang_hombro, 'codo': ang_codo}, elbow, wrist)
 
 def _calcular_distancia_mano(hand_landmarks, punto1=5, punto2=17):
@@ -307,7 +307,7 @@ def _calcular_angulos_dedos(hand_landmarks):
     try:
         angulo_anular = _calcular_angulo_3p(lm[13], lm[14], lm[16])
         dedos['anular'] = np.interp(angulo_anular, [INPUT_ANGLE_MIN, INPUT_ANGLE_MAX], 
-                                     [SERVO_ANGLE_MIN, SERVO_ANGLE_MAX])
+                                    [SERVO_ANGLE_MAX, SERVO_ANGLE_MIN])
     except Exception:
         dedos['anular'] = 90
 
